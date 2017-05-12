d6240d9754c7c656c9ce4478720071e6932b5b0b
Kotaro Abe <abekoh.bcky@gmail.com>
2017-05-13 03:11:27 +0900

    update around keras

--- cnn.py
+++ cnn.py
@@ -9,41 +9,32 @@ from keras.layers.core import Flatten
 from keras.layers.core import Dense
 
 # 参考:http://www.pyimagesearch.com/2016/08/01/lenet-convolutional-neural-network-in-python/
-class LeNet:
+class LeNet(Sequential):
     def __init__(self, width, height, depth, classes, weight_path=None):
-        self.width = width
-        self.height = height
-        self.depth = depth
-        self.classes = classes
-        self.weight_path = weight_path
-        self._build()
-
-    def _build(self):
-        # モデル初期化
-        self.model = Sequential()
+        super(LeNet, self).__init__()
         # first set of CONV => RELU => POOL
-        self.model.add(Convolution2D(20, 5, 5, border_mode='same', input_shape=(self.depth, self.height, self.width)))
-        self.model.add(Activation('relu'))
-        self.model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        self.add(Convolution2D(20, 5, 5, border_mode='same', input_shape=(depth, height, width)))
+        self.add(Activation('relu'))
+        self.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
         # second set of CONV => RELU => POOL
-        self.model.add(Convolution2D(50, 5, 5, border_mode='same'))
-        self.model.add(Activation('relu'))
-        self.model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        self.add(Convolution2D(50, 5, 5, border_mode='same'))
+        self.add(Activation('relu'))
+        self.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
         # set of FC => RELU layers
-        self.model.add(Flatten())
-        self.model.add(Dense(500))
-        self.model.add(Activation('relu'))
+        self.add(Flatten())
+        self.add(Dense(500))
+        self.add(Activation('relu'))
         # softmax clasifier
-        self.model.add(Dense(self.classes))
-        self.model.add(Activation('softmax'))
-        if self.weight_path is not None:
-            self.model.load_weights(self.weight_path)
+        self.add(Dense(classes))
+        self.add(Activation('softmax'))
+        if weight_path is not None:
+            self.load_weights(weight_path)
 
     def get_likelihoods(self, img_np):
         img_cnn = img_np.copy()
         img_cnn = img_cnn.astype(np.float32) / 255.0
         img_cnn = img_cnn[np.newaxis, np.newaxis, :, :]
-        pred = self.model.predict(img_cnn, batch_size=128, verbose=0)
+        pred = self.predict(img_cnn, batch_size=128, verbose=0)
         likelihoods = pred[0]
         return likelihoods
 
